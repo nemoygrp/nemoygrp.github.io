af1bd72c0cb3f0e60947829bdabe0a8eee427982
nemogrp <nemoygrp@gmail.com>
2018-09-27 18:41:05 +0300

    ver 1.0

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Main</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<div class="back">
+		<div class="logo">
+			<img src="img/by_NEMOY.png">
+		</div>
+
+		<img  src="img/fon_main.jpg">
+</div>
+		<div class="container"  id="contain">
+			<div class="header">
+				<ul class="menu">
+					<li><a style="text-shadow: 0 0 .65px #870D04, 0 0 .65px #870D04" href="index.html#contain">HOME</a> </li>
+					<li><a href="verstka.html#contain">HTML/CSS</a></li>
+					<li><a href="programs.html#contain">JScript/PHP</a> </li>
+					<li><a href="#contain">OTHER</a></li>
+					<li><a href="about.html#contain">ABOUT ME</a> </li>
+				</ul>
+
+			</div>
+
+			<div class="content" style="height: 200px">
+<h1 style="text-align: center">Приветствую!</h1>
+<p style="text-align: center"> На этом сайте собраны все работы, которые я делаю в рамках обучения на GeekBrains. <br> По мере изготовления, сайт будет дополняться и видоизменяться. <br> Для удобства доступа, я разбил меню, условно, на несколько разделов.  </p>
+			</div> 
+			<div class="clr" style="height: 400px;"></div>
+			<div class="footer">
+<span>Все права защищены &copy <?php echo "Привет";?> ООО "NemoyGroup"</span>
+			</div>
+		</div>
+		
+
+	</body>
+	</html>
